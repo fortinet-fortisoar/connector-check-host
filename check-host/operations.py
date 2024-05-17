2ff23970d72935f24d53f6cb8ff690f152acea63
cs-shivaji-kolape <skolape@fortinet.com>
2024-05-17 14:33:46 +0530

    reformat connector code

--- check-host/operations.py
+++ check-host/operations.py
@@ -13,14 +13,14 @@ logger = get_logger('check-host')
 class CheckHost(object):
 
     def __init__(self, config):
-        self.server_url = config.get('server_url').strip()
+        self.server_url = config.get('server_url','').strip('/')
         self.headers = {'accept': 'application/json'}
-        if not self.server_url.startswith('https://'):
-            self.server_url = 'https://{0}/'.format(self.server_url)
+        if not self.server_url.startswith('https://') and not self.server_url.startswith('http://'):
+            self.server_url = 'https://' + self.server_url
 
     def make_api_call(self, endpoint=None, method='GET', headers=None, health_check=False):
         url = self.server_url + endpoint
-        logger.debug('Final url to make rest call is: {0}'.format(url))
+        logger.error('RESP API Endpoint: {0}'.format(url))
         if headers:
             self.headers.update(headers)
         try:
@@ -41,9 +41,9 @@ class CheckHost(object):
                     logger.error('Failed with an error: {0}. The response details are: {1}'.format(e, response_data))
                     return {'status': 'Failure', 'data': response_data}
             else:
-                logger.error('Failed with response {0}'.format(response))
+                logger.error('Failed with response {0}'.format(response.text))
                 raise ConnectorError(
-                    {'status': 'Failure', 'status_code': str(response.status_code), 'response': response})
+                    {'status': 'Failure', 'status_code': str(response.status_code), 'response': response.text})
         except Exception as e:
             logger.exception(str(e))
             raise ConnectorError(str(e))
@@ -51,37 +51,37 @@ class CheckHost(object):
 
 def ping_check(config, params):
     obj = CheckHost(config)
-    endpoint = f"check-ping?host=check{params.get('host')}&max_nodes={params.get('max_nodes')}"
+    endpoint = f"/check-ping?host={params.get('host')}&max_nodes={params.get('max_nodes')}"
     return obj.make_api_call(endpoint=endpoint, health_check=True)
 
-            
+
 def http_check(config, params):
     obj = CheckHost(config)
-    endpoint = f"check-http?host=check{params.get('host')}&max_nodes={params.get('max_nodes')}"
+    endpoint = f"/check-http?host={params.get('host')}&max_nodes={params.get('max_nodes')}"
     return obj.make_api_call(endpoint=endpoint, health_check=True)
 
 
 def tcp_connection_check(config, params):
     obj = CheckHost(config)
-    endpoint = f"check-tcp?host=check{params.get('host')}&max_nodes={params.get('max_nodes')}"
+    endpoint = f"/check-tcp?host={params.get('host')}&max_nodes={params.get('max_nodes')}"
     return obj.make_api_call(endpoint=endpoint, health_check=True)
 
 
 def dns_address_check(config, params):
     obj = CheckHost(config)
-    endpoint = f"check-dns?host=check{params.get('host')}&max_nodes={params.get('max_nodes')}"
+    endpoint = f"/check-dns?host={params.get('host')}&max_nodes={params.get('max_nodes')}"
     return obj.make_api_call(endpoint=endpoint, health_check=True)
 
 
 def check_result_by_request_id(config, params):
     obj = CheckHost(config)
-    return obj.make_api_call(endpoint=f"check-result/{params.get('request_id')}", health_check=True)
+    return obj.make_api_call(endpoint=f"/check-result/{params.get('request_id')}", health_check=True)
 
 
 def _check_health(config):
     try:
         obj = CheckHost(config)
-        obj.make_api_call(endpoint='check-http?host=check-host.net&max_nodes=3', health_check=True)
+        obj.make_api_call(endpoint='/check-http?host=check-host.net&max_nodes=3', health_check=True)
         return True
     except Exception as err:
         logger.exception('Health check failed with: {0}'.format(err))
